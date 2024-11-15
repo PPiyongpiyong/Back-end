ff66ac6b19c944c0753bd600cc5c4d5a333806a2
Choi Minjune <minjune0807@gmail.com>
2024-11-16 01:36:32 +0900

    init: s3초기세팅

---
+++ springserver/src/main/java/com/example/springserver/SpringserverApplication.java
@@ -0,0 +1,15 @@
+package com.example.springserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 스프링 부트 애플리케이션의 진입점입니다.
+ */
+@SpringBootApplication
+public class SpringserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringserverApplication.class, args);
+	}
+
+}
