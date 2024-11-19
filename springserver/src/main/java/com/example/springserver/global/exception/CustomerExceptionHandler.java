c650276991412ffe96772e5d402a2282f6d44e1a
kite_U <109857975+youyeon11@users.noreply.github.com>
2024-11-19 23:49:12 +0900

    [feat] user 예외 기능 추가

--- springserver/src/main/java/com/example/springserver/global/exception/CustomerExceptionHandler.java
+++ springserver/src/main/java/com/example/springserver/global/exception/CustomerExceptionHandler.java
@@ -3,7 +3,6 @@ package com.example.springserver.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.View;
@@ -17,9 +16,9 @@ public class CustomerExceptionHandler {
     @ExceptionHandler(AbstractException.class)
     protected ResponseEntity<ErrorResponse> handleCustomException(AbstractException e) {
         ErrorResponse errorResponse = ErrorResponse.builder()
-                .code(e.getStatusCode())
-                .message(e.getMessage())
-                .build();
+                                        .code(e.getStatusCode())
+                                        .message(e.getMessage())
+                                        .build();
 
         return new ResponseEntity<>(errorResponse, HttpStatus.resolve(e.getStatusCode()));
     }
