fd06540b3f3aca831efcad99ba6aebd4f624e32b
kite_U <109857975+youyeon11@users.noreply.github.com>
2024-11-20 13:10:00 +0900

    feat: Auth model 추가

--- springserver/src/main/java/com/example/springserver/api/MyPage/Domain/MemberEntity.java
+++ springserver/src/main/java/com/example/springserver/api/MyPage/Domain/MemberEntity.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@NoArgsConstructor
 public class MemberEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
