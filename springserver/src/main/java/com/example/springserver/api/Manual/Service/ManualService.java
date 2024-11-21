a5cf0cea293eab6c6bf411d50784cfbc2f6b22c9
kite_U <109857975+youyeon11@users.noreply.github.com>
2024-11-21 12:53:19 +0900

    feat: 검색어 자동완성 기능 추가

--- springserver/src/main/java/com/example/springserver/api/Manual/Service/ManualService.java
+++ springserver/src/main/java/com/example/springserver/api/Manual/Service/ManualService.java
@@ -6,18 +6,49 @@ import com.example.springserver.api.Manual.Dto.ManualRespond.ManualRespondDto;
 import com.example.springserver.api.Manual.Repository.ManualRepository;
 import com.example.springserver.api.Manual.exception.ManualNotFoundException;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.Trie;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class ManualService {
+
+    private final Trie trie;
     private final ManualRepository manualRepository;
 
-
+    // 매뉴얼 이름으로 조회
     public ManualRespondDto getManualByEmergencyName(String emergencyName) {
         Manual manual = manualRepository.findByEmergencyName(emergencyName)
                 .orElseThrow(() -> new ManualNotFoundException("매뉴얼을 찾을 수 없습니다: " + emergencyName));
         return new ManualRespondDto(manual.getEmergencyName(), manual.getManualSummary());
+    }
+
+    // Trie에 키워드 저장 메서드
+    public void addAutocompleteKeyword(String keyword) {
+        this.trie.put(keyword, null); // key에는 입력 keyword, value는 null
+    }
+
+    // DB에 있는 매뉴얼 emergencyName 가져오기
+    public void loadEmergencyNameIntoTrie() {
+        List<String> emergencyNames = manualRepository.findAllEmergencyNames();
+        for (String name: emergencyNames) {
+            addAutocompleteKeyword(name);
+        }
+    }
+
+    // 저장한 Trie에서 이름 조회
+    public List<String> autocomplete(String keyword) {
+        return (List<String>) this.trie.prefixMap(keyword).keySet()
+                .stream().collect(Collectors.toList()); // 받아온 것을 list로 변환
+    }
+
+    // Trie에 저장된 키워드 삭제
+    public void deleteAutocompleteKeyword(String keyword) {
+        this.trie.remove(keyword);
     }
 }
