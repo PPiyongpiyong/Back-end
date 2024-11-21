21e8b366f03218699235af943a715c8eaa6c42ba
Choi Minjune <minjune0807@gmail.com>
2024-11-22 00:52:29 +0900

    feat: 카데고리별 조회 api구현

--- springserver/src/main/java/com/example/springserver/api/Manual/Repository/ManualCategoryRepository.java
+++ springserver/src/main/java/com/example/springserver/api/Manual/Repository/ManualCategoryRepository.java
@@ -5,11 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
-
-@Repository
-public interface ManualRepository extends JpaRepository<Manual, Long> {
-    Optional<Manual> findByEmergencyName(String emergencyName);
 
 
+public interface ManualCategoryRepository extends JpaRepository<Manual, Long> {
+    List<Manual> findByCategory(String category);  // 카테고리로 매뉴얼 찾기
 }
