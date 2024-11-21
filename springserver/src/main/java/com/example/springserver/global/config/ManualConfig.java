867ab82f525e5e3411df421a883a3e6f8732cc2e
kite_U <109857975+youyeon11@users.noreply.github.com>
2024-11-21 12:23:32 +0900

    fix: configuration 충돌 문제 해결(Bean 인식 중복)

--- springserver/src/main/java/com/example/springserver/global/config/ManualConfig.java
+++ springserver/src/main/java/com/example/springserver/global/config/ManualConfig.java
@@ -1,4 +1,4 @@
-package com.example.springserver.api.Manual.Config;
+package com.example.springserver.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
