bbfe4f65eb5ab8be8c874e0f1aad1378b545d02e
LHL <lin1137001@gmail.com>
2024-11-26 00:22:27 +0900

    hospitalmap

--- springserver/src/main/java/com/example/springserver/api/EmergencyMap/Dto/kakaoRestApi/KakaoCategorySearchResponse.java
+++ springserver/src/main/java/com/example/springserver/api/EmergencyMap/Dto/kakaoRestApi/KakaoCategorySearchResponse.java
@@ -1,5 +1,6 @@
 package com.example.springserver.api.EmergencyMap.Dto.kakaoRestApi;
 
+import com.example.springserver.api.EmergencyMap.Dto.HospitalSearchResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
