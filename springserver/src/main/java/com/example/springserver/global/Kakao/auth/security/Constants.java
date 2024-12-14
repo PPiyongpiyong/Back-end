3eb129618eefc63717b7e0efe306652b0f841a0a
Choi Minjune <minjune0807@gmail.com>
2024-12-15 05:49:22 +0900

    feat: http에 따른 해더부분 및 기타 부분 상수 데이터 선언

--- springserver/src/main/java/com/example/springserver/global/Kakao/auth/security/Constants.java
+++ springserver/src/main/java/com/example/springserver/global/Kakao/auth/security/Constants.java
@@ -3,4 +3,8 @@ package com.example.springserver.global.Kakao.auth.security;
 public class Constants {
     public static final String CHARACTER_TYPE = "UTF-8";
 
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+
+
 }
