922d817b7d4a056ed92f015dcb3a1955cf052855
Choi Minjune <minjune0807@gmail.com>
2024-12-15 03:10:48 +0900

    feat: 에러 코드 작성

--- springserver/src/main/java/com/example/springserver/global/Kakao/auth/client/client.java
+++
@@ -1,4 +0,0 @@
-package com.example.springserver.global.Kakao.auth.client;
-
-public class client {
-}
--- springserver/src/main/java/com/example/springserver/global/Kakao/auth/security/ExceptionHandlerFilter.java
+++ springserver/src/main/java/com/example/springserver/global/Kakao/auth/security/ExceptionHandlerFilter.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
