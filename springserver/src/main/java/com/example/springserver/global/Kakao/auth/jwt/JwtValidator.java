bb622b9461f57b0e0488b86322bc02954166b55d
Choi Minjune <minjune0807@gmail.com>
2024-12-15 03:31:47 +0900

    refactor: 에러코드 네이밍 변경

--- springserver/src/main/java/com/example/springserver/global/Kakao/auth/jwt/JwtValidator.java
+++ springserver/src/main/java/com/example/springserver/global/Kakao/auth/jwt/JwtValidator.java
@@ -1,10 +1,13 @@
 package com.example.springserver.global.Kakao.auth.jwt;
 
-import com.example.springserver.api.EmergencyMap.exception.UnauthorizedException;
+
+import com.example.springserver.global.Kakao.auth.Error.ErrorStatus;
+import com.example.springserver.global.Kakao.auth.Error.KaKaoUnauthorizedException;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
 
 @RequiredArgsConstructor
 @Component
@@ -16,9 +19,9 @@ public class JwtValidator {
             JwtParser jwtParser = jwtGenerator.getJwtParser();
             jwtParser.parseClaimsJws(accessToken);
         } catch (ExpiredJwtException e) {
-            throw new UnauthorizedException(ErrorStatus.EXPIRED_ACCESS_TOKEN);
+            throw new KaKaoUnauthorizedException(ErrorStatus.EXPIRED_ACCESS_TOKEN);
         } catch (Exception e) {
-            throw new UnauthorizedException(ErrorStatus.INVALID_ACCESS_TOKEN_VALUE);
+            throw new KaKaoUnauthorizedException(ErrorStatus.INVALID_ACCESS_TOKEN_VALUE);
         }
     }
 
@@ -27,15 +30,15 @@ public class JwtValidator {
             JwtParser jwtParser = jwtGenerator.getJwtParser();
             jwtParser.parseClaimsJws(refreshToken);
         } catch (ExpiredJwtException e) {
-            throw new UnauthorizedException(ErrorStatus.EXPIRED_REFRESH_TOKEN);
+            throw new KaKaoUnauthorizedException(ErrorStatus.EXPIRED_REFRESH_TOKEN);
         } catch (Exception e) {
-            throw new UnauthorizedException(ErrorStatus.INVALID_REFRESH_TOKEN_VALUE);
+            throw new KaKaoUnauthorizedException(ErrorStatus.INVALID_REFRESH_TOKEN_VALUE);
         }
     }
 
     public void equalsRefreshToken(String refreshToken, String storedRefreshToken) {
         if (!refreshToken.equals(storedRefreshToken)) {
-            throw new UnauthorizedException(ErrorStatus.NOT_MATCH_REFRESH_TOKEN);
+            throw new KaKaoUnauthorizedException(ErrorStatus.NOT_MATCH_REFRESH_TOKEN);
         }
     }
 }
