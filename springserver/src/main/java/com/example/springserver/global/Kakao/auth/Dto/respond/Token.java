6308beb177c1b833fd20faa6b3ddedec388c4c92
Choi Minjune <minjune0807@gmail.com>
2024-12-18 03:05:54 +0900

    fix: 카카오 서비스 유저 오류 해결

--- springserver/src/main/java/com/example/springserver/global/Kakao/auth/Dto/respond/Token.java
+++ springserver/src/main/java/com/example/springserver/global/Kakao/auth/Dto/respond/Token.java
@@ -2,17 +2,22 @@ package com.example.springserver.global.Kakao.auth.Dto.respond;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+
 
 @Builder(access = AccessLevel.PRIVATE)
 public record Token(
         String accessToken,
         String refreshToken
 ) {
+
     public static Token of(String accessToken, String refreshToken) {
+
         return Token.builder()
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .build();
     }
+
 }
 
