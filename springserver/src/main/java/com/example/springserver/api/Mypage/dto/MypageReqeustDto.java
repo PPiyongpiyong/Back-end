9266c16cf82adf5597d156aa2948470b6be5e612
kite_U <yurry45@naver.com>
2024-12-29 17:15:52 +0900

    feat: 마이페이지 조회 및 수정 기능 추가

---
+++ springserver/src/main/java/com/example/springserver/api/Mypage/dto/MypageReqeustDto.java
@@ -0,0 +1,9 @@
+package com.example.springserver.api.Mypage.dto;
+
+public class MypageReqeustDto {
+    // 수정 가능한 내용들
+    private String username;
+    private String address;
+    private String phoneNumber;
+    private String parentPhoneNumber;
+}
