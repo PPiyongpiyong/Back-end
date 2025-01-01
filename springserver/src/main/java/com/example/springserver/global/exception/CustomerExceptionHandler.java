0400a59e74992084c2cd48792875ae9aeec70aa4
kite_U <yurry45@naver.com>
2025-01-02 05:20:26 +0900

    feat: 인증 및 인가 예외 custom

--- springserver/src/main/java/com/example/springserver/global/exception/CustomerExceptionHandler.java
+++ springserver/src/main/java/com/example/springserver/global/exception/CustomerExceptionHandler.java
@@ -14,12 +14,6 @@ import java.nio.file.AccessDeniedException;
 @ControllerAdvice
 public class CustomerExceptionHandler {
 
-    private final View error;
-
-    public CustomerExceptionHandler(View error) {
-        this.error = error;
-    }
-
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e, HttpServletRequest request) {
 
@@ -28,18 +22,5 @@ public class CustomerExceptionHandler {
         log.error("Error message : {}", errorCode.getDescription());
 
         return ErrorResponse.toResponseEntity(e.getErrorCode());
-    }
-
-    @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
-        log.error("Access denied", e);
-
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                                                .status(HttpStatus.FORBIDDEN.value())
-                                                .message(e.getMessage())
-                                                .build();
-
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                            .body(errorResponse);
     }
 }
