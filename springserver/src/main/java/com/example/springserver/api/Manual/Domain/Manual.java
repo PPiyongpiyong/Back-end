53252dab87d9b56a927b3dbf87696cc23b7bf943
Choi Minjune <minjune0807@gmail.com>
2025-01-02 00:34:53 +0900

    test:ddj

--- springserver/src/main/java/com/example/springserver/api/Manual/Domain/Manual.java
+++ springserver/src/main/java/com/example/springserver/api/Manual/Domain/Manual.java
@@ -8,7 +8,7 @@ import lombok.*;
 @Setter// 모든 필드에 대해 getter 메소드를 자동으로 생성
 @NoArgsConstructor // 매개변수가 없는 기본 생성자를 자동으로 생성 (new Bank())
 @AllArgsConstructor // 모든 필드를 매개변수로 받는 전체 생성자를 자동으로 생성
-@Builder //
+@Builder //dd
 @Entity
 public class Manual {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
