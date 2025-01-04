4abd2d5e16c5b0415831767e855617b085727f87
kite_U <yurry45@naver.com>
2025-01-04 13:33:42 +0900

    fix: CI 중복 파일 오류 수정

--- springserver/src/main/java/com/example/springserver/global/kakao/controller/KakaoLoginController.java
+++ springserver/src/main/java/com/example/springserver/global/kakao/controller/KakaoLoginController.java
@@ -3,11 +3,12 @@ package com.example.springserver.global.kakao.controller;
 import com.example.springserver.api.Mypage.repository.MemberRepository;
 import com.example.springserver.global.kakao.dto.KakaoUserInfoResponseDto;
 import com.example.springserver.global.kakao.service.KakaoService;
+import com.example.springserver.global.security.dto.MemberResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +22,18 @@ public class KakaoLoginController {
     private final KakaoService kakaoService;
     private final MemberRepository memberRepository;
 
-    @Operation(summary = "카카오 로그인")
+    @Operation(summary = "카카오 로그인", description = """
+            카카오 로그인을 통하여 유저의 이름과, 이메일을 저장합니다.<br>
+            구체적인 회원정보는 마이페이지 수정에서 처리합니다.
+            """, parameters = @Parameter(name = "Code", description = "카카오로부터 받은 인가 코드"))
     @PostMapping("/callback")
-    public String callback(String code) {
+    public ResponseEntity<?> callback(
+            String code) {
         String accessToken = kakaoService.getAccessTokenFromKakao(code).access();
 
-        kakaoService.getUserInfo(code);
-        return "access_token=" + accessToken;
+        KakaoUserInfoResponseDto userInfo = kakaoService.getUserInfo(accessToken);
+
+        MemberResponseDto responseDto = kakaoService.saveMemberInfo(userInfo);
+        return ResponseEntity.ok(responseDto);
     }
 }
