d0bc2c32b85be00b57ed4916e939ee17bf07eec6
Choi Minjune <minjune0807@gmail.com>
2025-01-08 19:28:30 +0900

    refactor: cors수정

--- springserver/src/main/java/com/example/springserver/global/config/SecurityConfig.java
+++ springserver/src/main/java/com/example/springserver/global/config/SecurityConfig.java
@@ -18,6 +18,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -66,9 +68,12 @@ public class SecurityConfig {
 
         CorsConfiguration conf = new CorsConfiguration();
 
-        conf.addAllowedOrigin("*"); // 모든 Origin 허용
-        conf.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
-        conf.addAllowedHeader("*"); // 모든 헤더 허용
+        conf.setAllowedOrigins(List.of("http://52.79.245.244:8080"));
+        // 모든 Origin 허용
+        conf.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));// 모든 HTTP 메서드 허용
+        // 허용할 HTTP 헤더 (필요한 헤더만 추가)
+        conf.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        // 모든 헤더 허용
         conf.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
