287bca8a0570aae7ecb017b0257d20e05b3e7ba8
kite_U <yurry45@naver.com>
2025-01-23 03:31:10 +0900

    feat: 카카오 로그인 성공시 JWT토큰 발급받기

--- springserver/src/main/java/com/example/springserver/global/kakao/controller/KakaoLoginController.java
+++ springserver/src/main/java/com/example/springserver/global/kakao/controller/KakaoLoginController.java
@@ -1,9 +1,13 @@
 package com.example.springserver.global.kakao.controller;
 
+import com.example.springserver.api.Mypage.domain.MemberEntity;
 import com.example.springserver.api.Mypage.repository.MemberRepository;
+import com.example.springserver.global.auth.TokenProvider;
 import com.example.springserver.global.kakao.dto.KakaoUserInfoResponseDto;
 import com.example.springserver.global.kakao.service.KakaoService;
 import com.example.springserver.global.security.dto.MemberResponseDto;
+import com.example.springserver.global.security.dto.TokenDto;
+import com.example.springserver.global.security.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -23,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class KakaoLoginController {
 
     private final KakaoService kakaoService;
+    private final TokenProvider tokenProvider;
 
     @Operation(summary = "카카오 인가 코드 받기", description = """
             카카오 인가 코드를 받기 위한 url을 반환합니다.
@@ -35,6 +40,7 @@ public class KakaoLoginController {
     @Operation(summary = "카카오 로그인", description = """
             카카오 로그인을 통하여 유저의 이름과, 이메일을 저장합니다.<br>
             인가 코드 요청 url을 통하여 코드를 받습니다.<br>
+            로그인 성공시 토큰을 발급합니다.
             구체적인 회원정보는 마이페이지 수정에서 처리합니다.
             """, parameters = @Parameter(name = "Code", description = "카카오로부터 받은 인가 코드"))
     @PostMapping("/callback")
@@ -44,7 +50,10 @@ public class KakaoLoginController {
 
         KakaoUserInfoResponseDto userInfo = kakaoService.getUserInfo(accessToken);
 
-        MemberResponseDto responseDto = kakaoService.saveMemberInfo(userInfo);
-        return ResponseEntity.ok(responseDto);
+        MemberEntity member = kakaoService.saveMemberInfo(userInfo);
+
+        TokenDto tokenDto = kakaoService.generateToken(member);
+
+        return ResponseEntity.ok(tokenDto);
     }
 }
