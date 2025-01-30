c140503fac0c999b771d0b3ccc1ce2fc35638718
Choi Minjune <minjune0807@gmail.com>
2025-01-30 19:57:28 +0900

    feat: actuator 추가

---
+++ springserver/build/resources/main/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO Manual (category, emergency_name, manual_detail, manual_summary, keyword, imgurl)
+VALUES
+    ('3. 의학적', '심정지(비외상성)',
+     '1. 심정지 확인과 심장충격기 준비\n심정지 확인: 환자가 반응이 없고, 숨을 쉬지 않으면 심정지로 간주합니다.\n자동심장충격기(AED) 준비: 주변에 AED가 있으면 즉시 가져옵니다.',
+     '심정지 응급 대처',
+     '심정지, CPR, AED 사용법',
+     'https://ppiyongppiyong.s3.ap-northeast-2.amazonaws.com/심정지(비외상성).jpeg'),
+
+    ('3. 의학적', '심정지(외상성)',
+     '1. 현장 안전 및 감염 방지\n심정지 확인: 주변이 안전한지 확인하세요 (화재, 전기 위험, 교통사고 등).',
+     '외상성 심정지 응급 대처',
+     '외상성 심정지, 감염 방지',
+     'https://ppiyongppiyong.s3.ap-northeast-2.amazonaws.com/심정지(외상성).jpeg');
